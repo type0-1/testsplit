9fef18e202ed13be3e791cf9e8c90ab7789de7e2
Samson Oloruntola <samson.oloruntola2@mail.dcu.ie>
2026-01-02 13:13:53 +0000

    chore(storage): Update test engine to incorporate storage persistence and update corresponding test

--- src/backend/core/TestSplitEngine.ts
+++ src/backend/core/TestSplitEngine.ts
@@ -2,17 +2,29 @@ import { parseJUnitXML } from '../parser/JUnitXMLParser';
 import { Profiler } from '../profiler/core/Profiler';
 import { LPTScheduler } from '../algorithm/core/LPTScheduler';
 import { Task } from '../algorithm/model/Task';
-
+import { FileStore } from '../storage/FileStore';
+import { generateRunId } from '../helpers/RunId'
 export class TestSplitEngine {
   private profiler = new Profiler();
   private scheduler = new LPTScheduler();
+  private store = new FileStore();
 
-  run(xmlPath: string, jobCount: number) {
+  constructor(baseDir?: string){
+    this.store = new FileStore(baseDir);
+  }
+
+  run(xmlPath: string, jobCount: number, persist: boolean) {
     const results = parseJUnitXML(xmlPath);
     const profile = this.profiler.generateProfile(results);
     const tasks: Task[] = profile.testResults.map(r => ({id: r.name, duration: r.duration})); // Map tasks to profiler
     const distribution = this.scheduler.schedule(tasks, jobCount); // Schedule tasks
+    const runId = generateRunId();
 
-    return { profile, distribution };
+    if(persist){
+      this.store.saveProfile(runId, profile)
+      this.store.saveDistribution(runId, distribution)
+    }
+
+    return { runId, profile, distribution };
   }
 }
