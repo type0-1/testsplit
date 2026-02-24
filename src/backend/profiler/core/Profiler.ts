ce4e5bbcc6836ae020eb41776aa6d1abd359de0b
Samson Oloruntola <samson.oloruntola2@mail.dcu.ie>
2026-02-24 10:58:01 +0000

    chore(profiler): Remove unused imports

--- src/backend/profiler/core/Profiler.ts
+++ src/backend/profiler/core/Profiler.ts
@@ -2,11 +2,9 @@ import { CommitInfo, CommitTracker } from '../../helpers/CommitTracker';
 import { TestResult } from '../../models/TestResult';
 import { Profile } from '../model/Profile';
 import { validateResults } from '../validation/ProfilerValidator';
-import { ProfileMetadata } from '../model/Profile';
 import { PROFILE_SCHEMA_VERSION } from '../../storage/SchemaVersions';
 import { EnvironmentCollector } from './EnvironmentCollector';
 
-import os from 'os';
 export class Profiler {
   generateProfile(results: TestResult[], commit?: CommitInfo): Profile {
     validateResults(results);
