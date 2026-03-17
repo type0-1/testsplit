ed92ec597435654c5380dd38f3854ee0e6ea7c3c
Samson Oloruntola <samson.oloruntola2@mail.dcu.ie>
2026-03-17 20:16:35 +0000

    ci: add JUnit reports, coverage threshold, and e2e rules

--- jest.base.config.js
+++ jest.base.config.js
@@ -13,4 +13,21 @@ module.exports = {
   collectCoverageFrom: ['src/backend/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    './src/backend/': {
+      lines: 80,
+      functions: 80,
+      branches: 70,
+      statements: 80,
+    },
+  },
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'coverage',
+      outputName: 'junit.xml',
+      classNameTemplate: '{classname}',
+      titleTemplate: '{title}',
+    }],
+  ],
 };
